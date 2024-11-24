b92dae0bba2b79c87072bc92853e8d3847a648ac
3107.Pucc <ndp112005@gmail.com>
2024-11-25 01:04:01 +0700

    Sửa sản phẩm và album ảnh sản phẩm

--- admin/controllers/AdminSanPhamController.php
+++ admin/controllers/AdminSanPhamController.php
@@ -139,52 +139,198 @@ class AdminSanPhamController {
         }
     }
 
-    // public function formEditDanhMuc() {
-    //     // Ham nay dung de hien thi form suaaa
-    //     // Lay ra thong tin cua danh muc can sua
-    //     $id = $_GET['id_danh_muc'];
-    //     $danhMuc = $this->modelDanhMuc->getDetailDanhMuc($id);
-    //     if ($danhMuc) {
-    //         require_once './views/danhmuc/editDanhMuc.php';
-    //     } else {
-    //         header("Location: " . BASE_URL_ADMIN . '?act=danh-muc');
-    //         exit();
-    //     }
-    // }
+    public function formEditSanPham() {
+        // Ham nay dung de hien thi form suaaa
+        // Lay ra thong tin cua danh muc can sua
+        $id = $_GET['id_san_pham'];
+        $sanPham = $this->modelSanPham->getDetailSanPham($id);
+        $listAnhSanPham = $this->modelSanPham->getListAnhSanPham($id);
+        $listDanhMuc = $this->modelDanhMuc->getAllDanhMuc();
+        if ($sanPham) {
+            require_once './views/sanpham/editSanPham.php';
+            deleteSessionError();
+        } else {
+            header("Location: " . BASE_URL_ADMIN . '?act=san-pham');
+            exit();
+        }
+    }
 
-    // public function postEditDanhMuc() {
-    //     // Ham nay dung de xu ly them du lieu
-    //     // var_dump($_POST);
+    public function postEditSanPham() {
+        // Ham nay de xu ly them du lieu
+        // var_dump($_POST);
 
-    //     // Kiem tra xem du lieu co duoc submit len kh
-    //     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    //         // Lay ra du lieu
-    //         $id = $_POST['id'];
-    //         $ten_danh_muc = $_POST['ten_danh_muc'];
-    //         $mo_ta = $_POST['mo_ta'];
+        // Kiem tra xem du lieu co duoc submit len kh
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // Lay ra du lieu
+            // Lay ra du lieu cu cua san pham
+            $san_pham_id = $_POST['san_pham_id'] ?? '';
 
-    //         // Tao 1 mang trong de chua du lieu
-    //         $errors = [];
-    //         if (empty($ten_danh_muc)) {
-    //             $errors['ten_danh_muc'] = 'Ten danh muc khong duoc de trong';
-    //         }
+            // Truy van
+            $sanPhamOld = $this->modelSanPham->getDetailSanPham($san_pham_id);
+            $old_file = $sanPhamOld['hinh_anh']; // Lay anh cu de phuc vu cho sua anh
 
-    //         // Neu khong co loi thi tien hanh sua danh muc
-    //         if (empty($errors)) {
-    //             // Neu khong co loi thi tien hanh sua danh muc
-    //             // var_dump('oke');
+            $ten_san_pham = $_POST['ten_san_pham'] ?? '';
+            $gia_san_pham = $_POST['gia_san_pham'] ?? '';
+            $gia_khuyen_mai = $_POST['gia_khuyen_mai'] ?? '';
+            $so_luong = $_POST['so_luong'] ?? '';
+            $ngay_nhap = $_POST['ngay_nhap'] ?? '';
+            $danh_muc_id = $_POST['danh_muc_id'] ?? '';
+            $trang_thai = $_POST['trang_thai'] ?? '';
+            $mo_ta = $_POST['mo_ta'] ?? '';
 
-    //             $this->modelDanhMuc->updateDanhMuc($id, $ten_danh_muc, $mo_ta);
-    //             header("Location: " . BASE_URL_ADMIN . '?act=danh-muc');
-    //             exit();
+            $hinh_anh = $_FILES['hinh_anh'] ?? null;
 
-    //         } else {
-    //             // Tra ve form vaf bao loi
-    //             $danhMuc = ['id' => $id, 'ten_danh_muc' => $ten_danh_muc, 'mo_ta' => $mo_ta];
-    //             require_once './views/danhmuc/editDanhMuc.php';
-    //         }
-    //     }
-    // }
+
+            // Tao 1 mang trong de chua du lieu
+            $errors = [];
+
+            if (empty($ten_san_pham)) {
+                $errors['ten_san_pham'] = 'Ten san pham khong duoc de trong';
+            }
+
+            if (empty($gia_san_pham)) {
+                $errors['gia_san_pham'] = 'Gia san pham khong duoc de trong';
+            }
+
+            if (empty($gia_khuyen_mai)) {
+                $errors['gia_khuyen_mai'] = 'Gia khuyen mai khong duoc de trong';
+            }
+
+            if (empty($so_luong)) {
+                $errors['so_luong'] = 'So luong khong duoc de trong';
+            }
+
+            if (empty($ngay_nhap)) {
+                $errors['ngay_nhap'] = 'Ngay nhap khong duoc de trong';
+            }
+
+            if (empty($danh_muc_id)) {
+                $errors['danh_muc_id'] = 'Dnah muc khong duoc de trong';
+            }
+
+            if (empty($trang_thai)) {
+                $errors['trang_thai'] = 'Trang thai san pham phai chon';
+            }
+
+            $_SESSION['error'] = $errors;
+
+
+            // logic sua anh
+            if (isset($hinh_anh) && $hinh_anh['error'] == UPLOAD_ERR_OK) {
+                // upload anh moi len
+                $new_file = upLoadFile($hinh_anh, './uploads');
+
+                if (!empty($old_file)) { // Neu co anh cu thi xoa di
+                    deleteFile($old_file);
+                }
+            }else{
+                $new_file = $old_file;
+            }
+
+            // Neu khong co loi thi tien hanh them san pham
+            if (empty($errors)) {
+                // Neu khong co loi thi tien hanh them san pham
+                // var_dump('oke');
+
+                $this->modelSanPham->updateSanPham(
+                                                   $san_pham_id, 
+                                                   $ten_san_pham, 
+                                                   $gia_san_pham, 
+                                                   $gia_khuyen_mai, 
+                                                   $so_luong,
+                                                   $ngay_nhap, 
+                                                   $danh_muc_id,
+                                                   $trang_thai,
+                                                   $mo_ta,
+                                                   $new_file
+                                                );
+                // var_dump($san_pham_id);die;
+                header("Location: " . BASE_URL_ADMIN . '?act=san-pham');
+                exit();
+
+            } else {
+                // Tra ve form vaf bao loi
+                // Dat chi thi xoa session sau khi hien thi form
+                $_SESSION['flash'] = true;
+
+                header("Location: " . BASE_URL_ADMIN . '?act=form-sua-san-pham&id_san_pham=' . $san_pham_id);
+                exit();
+            }
+        }
+    }
+
+    // // Sua album anh
+    // - Sua anh cu
+    //     + Them anh moi
+    //     + Kh them anh moi
+    // - Kh sua anh cu
+    //     + Them anh moi
+    //     + Kh them anh moi
+    // -Xoa anh cu
+    //     + Them anh moi
+    //     + Kh them anh moi
+
+    public function postEditAnhSanPham() {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $san_pham_id = $_POST['san_pham_id'] ?? '';
+
+            // Lay danh sach anh hien tai cua san pham
+            $listAnhSanPhamCurrent = $this->modelSanPham->getListAnhSanPham($san_pham_id);
+
+            // Xu ly cac anh duoc gui tu form
+            $img_array = $_FILES['img_array'];
+            $img_detele = isset($_POST['img_delete']) ? explode(',', $_POST['img_delete']) : [];
+            $current_img_ids = $_POST['current_img_ids'] ?? [];
+
+            // Khai bao mang de luu anh them moi hoac thay the anh cu
+            $upload_file = [];
+
+            // Up load anh moi hoac thay the anh cu
+            foreach ($img_array['name'] as $key => $value) {
+                if ($img_array['error'][$key] == UPLOAD_ERR_OK) {
+                   $new_file = uploadFileAlbum($img_array, './uploads/', $key);
+                   if ($new_file) {
+                        $upload_file[] = [
+                            'id' => $current_img_ids[$key] ?? null,
+                            'file' => $new_file
+                        ];
+                   }
+                }
+            }
+
+            // Luu anh moi vao db va xoa anh cu neu co
+            foreach($upload_file as $file_info) {
+                if ($file_info['id']) {
+                   $old_file = $this->modelSanPham->getDetailAnhSanPham($file_info['id'])['link_hinh_anh'];
+
+                    // Cap nhat anh cu
+                    $this->modelSanPham->updateAnhSanPham($file_info['id'], $file_info['file']);
+
+                    // Xoa anh cu
+                    deleteFile($old_file);
+                } else {
+                    // Them anh moi
+                    $this->modelSanPham->insertAlbumAnhSanPham($san_pham_id, $file_info['file']);
+                }
+            }
+
+            // Xu ly xoa anh 
+            foreach ($listAnhSanPhamCurrent as $anhSP) {
+                $anh_id = $anhSP['id'];
+                if (in_array($anh_id, $img_detele)) {
+                    // Xoa anh trong db
+                    $this->modelSanPham->destroyAnhSanPham($anh_id);
+
+                    // Xoa file
+                    deleteFile($anhSP['link_hinh_anh']);
+                }
+            }
+
+            header("Location: " . BASE_URL_ADMIN . '?act=form-sua-san-pham&id_san_pham=' . $san_pham_id);
+            exit();
+        }
+    }
+
 
     // public function deleteDanhMuc() {
     //     $id = $_GET['id_danh_muc'];
